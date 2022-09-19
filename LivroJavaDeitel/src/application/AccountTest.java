822d7d687399455751ddbd3e1638a0cca296e562
KiosaninMarc <marcelomarcweb@gmail.com>
2022-09-19 15:16:20 -0300

    Adição de try catch

--- LivroJavaDeitel/src/application/AccountTest.java
+++ LivroJavaDeitel/src/application/AccountTest.java
@@ -1,4 +1,5 @@
 package application;
+import java.util.InputMismatchException;
 //Figura 3.9: AccountTest.java entrada e saida de numeros de ponto flutuante com objetos Account
 import java.util.Scanner;
 import entities.Account;
@@ -17,33 +18,39 @@ public class AccountTest
 		
 		//cria um Scanner para obter entrada a partir da janela de comando
 		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Enter deposit amount for account1: "); //prompt
-		double depositAmount = input.nextDouble(); // obtém a entrada do usuário
-		System.out.printf("%nadding %.2f to account1 balance%n%n", 
+		Scanner sc = new Scanner(System.in);
+	
+		try {	
+			System.out.print("Enter deposit amount for account1: "); //prompt
+			double depositAmount = input.nextDouble(); // obtém a entrada do usuário
+			System.out.printf("%nadding %.2f to account1 balance%n%n", 
 					depositAmount);
-		account1.deposit(depositAmount); // adiciona o saldo de account1
-
+			account1.deposit(depositAmount); // adiciona o saldo de account1
+			
+			// exibe os saldos
+			System.out.printf("%s balance: $%.2f %n",
+					account1.getName(),account1.getBalance()); 
+			System.out.printf("%s balance: $%.2f %n%n",
+					account2.getName(),account2.getBalance()); 
+		}catch (InputMismatchException e) {
+			System.out.println("input error, account balance 1 not updated");
+		}
 		
-		// exibe os saldos
-		System.out.printf("%s balance: $%.2f %n",
-				account1.getName(),account1.getBalance()); 
-		System.out.printf("%s balance: $%.2f %n%n",
-				account2.getName(),account2.getBalance()); 
+		try {
+			System.out.print("\nEnter deposit amount for account2: "); // prompt
+			double depositAmount = sc.nextDouble(); // obtém a entrada do usuário
+			System.out.printf("%nadding %.2f to account2 balance%n%n", 
+					depositAmount);
+			account2.deposit(depositAmount); // adiciona ao saldo de account2
 		
-		System.out.print("Enter deposit amount for account2: "); // prompt
-		depositAmount = input.nextDouble(); // obtém a entrada do usuário
-		System.out.printf("%nadding %.2f to account2 balance%n%n", 
-				depositAmount);
-		account2.deposit(depositAmount); // adiciona ao saldo de account2
-
-		
-		//exibe os saldos
-		System.out.printf("%s balance: $%.2f %n",
-				account1.getName(),account1.getBalance()); 
-		System.out.printf("%s balance: $%.2f %n%n",
-				account2.getName(),account2.getBalance()); 
-		
-		input.close();
+			
+			//exibe os saldos
+			System.out.printf("%s balance: $%.2f %n",
+					account1.getName(),account1.getBalance()); 
+			System.out.printf("%s balance: $%.2f %n%n",
+					account2.getName(),account2.getBalance()); 
+		}catch (InputMismatchException e) {
+			System.out.println("input error, account balance 2 not updated");
+		}
 	}//fim de main
 }//Fim da classe AccountTest
