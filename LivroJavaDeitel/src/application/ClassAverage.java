184e0190bb1d8c38c0563599724f17502f165c66
KiosaninMarc <marcelomarcweb@gmail.com>
2022-10-06 05:08:05 -0300

    While com quantidade de loop controlado por sentinela

--- LivroJavaDeitel/src/application/ClassAverage.java
+++ LivroJavaDeitel/src/application/ClassAverage.java
@@ -1,33 +1,45 @@
 package application;
+
 //Figura 4.8: ClassAverage.java
 // Resolvendo o problema da média da classe usando a repetição controlada por contador.
 import java.util.Scanner;
+
 public class ClassAverage {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		// cria Scanner para obter entrada a partir da janela de comando
 		Scanner input = new Scanner(System.in);
 
 		// fase de inicialização
 		int total = 0; // inicializa a soma das notas inseridas pelo usuário
-		int gradeCounter = 1; // inicializa nº da nota a ser inserido em seguida
+		int gradeCounter = 0; // inicializa nº da nota a ser inserido em seguida
 
-		// fase de processamento utiliza repetição controlada por contador
-		while (gradeCounter <= 10) // faz o loop 10 vezes
-		{
-			System.out.print("Enter grade: "); // prompt
-			int grade = input.nextInt(); // insere a próxima nota
+		// fase de processamento
+		// solicita entrada e le a nota do usuario
+		System.out.print("Enter grade or -1 to quit: ");
+		int grade = input.nextInt();
+
+		// faz um loop até ler o valor de sentinela inserido pelo usuario
+		while (grade != -1) {
 			total = total + grade; // adiciona grade a total
-			gradeCounter = gradeCounter + 1; // incrementa o contador por 1ÿ
+			gradeCounter = gradeCounter + 1; // incrementa counter
+
+			// solicita entrada e lê a próxima nota fornecida pelo usuário
+			System.out.print("Enter grade or -1 to quit: ");
+			grade = input.nextInt();
 		}
 
 		// fase de término
-		int average = total / 10; // divisão de inteiros produz um resultado inteiro
+		// se usuário inseriu pelo menos uma nota...
+		if (gradeCounter != 0) {
+			// usa número com ponto decimal para calcular média das notasÿ
+			double average = (double) total / gradeCounter;
 
-		// exibe o total e a média das notas
-		System.out.printf("%nTotal of all 10 grades is %d%n", total);
-		System.out.printf("Class average is %d%n", average);
+			// exibe o total e a média (com dois dígitos de precisão)
+			System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+			System.out.printf("Class average is %.2f%n", average);
+		} else // nenhuma nota foi inserida, assim gera a saída da mensagem apropriada
+			System.out.println("No grades were entered");
 
 		input.close();
 	}
